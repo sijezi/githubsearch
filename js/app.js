4f0df4612c6a93bc933942d62ce4eb333350e81d
wehelie <leyth@leyth.local>
2015-10-11 00:48:30 -0700

    ajax called completed

--- js/app.js
+++ js/app.js
@@ -1,16 +1,35 @@
 
 function requestListener() {
-	console.log(this.responseText);
+	var req = JSON.parse(this.responseText);
+	console.log(req.id); 
 }
 
 
+// grab user input
 var userid = document.getElementById('user').value;
 
+// make new xmlhttp object
 var xmlhttp = new XMLHttpRequest();
 
+// request url
 var url = 'https://api.github.com/users/'+userid;
 
+// add event listener
 xmlhttp.addEventListener('load', requestListener);
+
+
+
+
+// open the request
 xmlhttp.open('GET', url, true);
+
+// send
 xmlhttp.send();
 
+var template = Handlebars.compile('<div>Hello {{name}}</div>');
+var html = document.getElementById('app');
+html.innerHTML = template({name: userid});
+
+function clickMe() {
+	document.getElementById("form").submit();
+}
